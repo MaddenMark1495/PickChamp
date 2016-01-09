57463978bb2b3115b1eb5fb0a64269e3d1d016f2
Mark Madden <mcmpw6@mail.missouri.edu>
2016-01-08 23:09:06 -0600

    Fail page

--- js/chart.js
+++ js/chart.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     $.ajax('chart.php', {
         data: {},
@@ -13,7 +14,7 @@ $(document).ready(function(){
                 {
                     _data.push(response[league][vars]);
                 }
-                //console.log(_data);return false;
+               
                 $('<div id="league-' + league + '" style="min-width: 310px; height: 500px; max-width: 600px; margin: 0 auto"></div>').appendTo($('#charts'));
                 $('#league-' + league).highcharts({
                     chart: {
@@ -45,82 +46,4 @@ $(document).ready(function(){
                         name: 'Picks',
                         colorByPoint: true,
                         data: _data
-                        /*data: [{
-                            name: 'Microsoft Internet Explorer',
-                            y: 56.33
-                        }, {
-                            name: 'Chrome',
-                            y: 24.03,
-                            sliced: true,
-                            selected: true
-                        }, {
-                            name: 'Firefox',
-                            y: 10.38
-                        }, {
-                            name: 'Safari',
-                            y: 4.77
-                        }, {
-                            name: 'Opera',
-                            y: 0.91
-                        }, {
-                            name: 'Proprietary or Undetectable',
-                            y: 0.2
-                        }]*/
-                    }]
-                });
-            }
-            
-            
-            /*$('#chart').highcharts({
-                chart: {
-                    type: 'column'
-                },
-                title: {
-                    text: 'Users picks'
-                },
-                xAxis: {
-                    categories: _weeks
-                    //categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-                },
-                yAxis: {
-                    //allowDecimals: false,
-                    min: 0,
-                    title: {
-                        text: 'Picks count'
-                    }
-                }/*,
-                tooltip: {
-                    formatter: function () {
-                        return '<b>' + this.x + '</b><br/>' +
-                            this.series.name + ': ' + this.y + '<br/>' +
-                            'Total: ' + this.point.stackTotal;
-                    }
-                },
-                plotOptions: {
-                    column: {
-                        stacking: 'normal'
-                    }
-                },
-                series: response.data
-                /*
-                series: [{
-                    name: 'John',
-                    data: [5, 3, 4, 7, 2],
-                    stack: 'male'
-                }, {
-                    name: 'Joe',
-                    data: [3, 4, 4, 2, 5],
-                    stack: 'male'
-                }, {
-                    name: 'Jane',
-                    data: [2, 5, 6, 2, 1],
-                    stack: 'female'
-                }, {
-                    name: 'Janet',
-                    data: [3, 0, 4, 4, 3],
-                    stack: 'female'
-                }]*/
-            //});
-        }
-    });
-});
+                        =
